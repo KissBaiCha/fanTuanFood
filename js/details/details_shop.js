71ecd953180192af287cb53c5e1ef73071c2d447
Asphyxia <1628776120@qq.com>
2022-10-08 21:25:50 +0800

    10.08-21.25-shangjian

--- js/details/details_shop.js
+++ js/details/details_shop.js
@@ -29,13 +29,13 @@ layui.use(['carousel', 'form'], function () {
 })
 // 店铺地图定位
 var map = new AMap.Map('container', {
-    zoom:20, //初始化地图层级
+    zoom:18, //初始化地图层级
     center: [120.693277,31.297477] //初始化地图中心点
 });
 // 定位图标
 var marker = new AMap.Marker({ 
     map: map,
     position: [120.693277, 31.297477],
-    icon: '../images/map-location-icon.png' ,
+    icon: '../images/map-location-icon.svg' ,
     offset: new AMap.Pixel(-13, -30)
 });
